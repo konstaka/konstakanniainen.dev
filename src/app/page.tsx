390d9f5a50c52604a5d2cde3a6ea64c357f46a70
Konsta Kanniainen <extkoka@lt1251.local>
2024-09-08 23:39:01 +0300

    input field focus logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 
 // import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import Sidebar from "@/components/sidebar";
 import Console from "@/components/console";
 
 export default function Home() {
+  const [inputInFocus, setInputInFocus] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputInFocus) {
+      inputRef.current?.focus();
+    }
+  }, [inputInFocus]);
+
   return (
-    <div className="flex h-screen font-[family-name:var(--font-geist-sans)]">
+    <div
+      className="flex h-screen font-[family-name:var(--font-geist-sans)]"
+      onClick={() => setInputInFocus(false)}
+    >
       <Sidebar />
       <div className="flex-auto p-12 shadow-xl">
-        <Console />
+        <Console inputRef={inputRef} setInputInFocus={setInputInFocus} />
       </div>
     </div>
   );
--- src/components/console.tsx
+++ src/components/console.tsx
@@ -1,14 +1,24 @@
-import { useRef } from "react";
+import { Dispatch, RefObject, SetStateAction } from "react";
 
-export default function Console() {
+export default function Console({
+  inputRef,
+  setInputInFocus,
+}: {
+  inputRef: RefObject<HTMLInputElement>;
+  setInputInFocus: Dispatch<SetStateAction<boolean>>;
+}) {
   const consoleContent: string[] = ["Welcome to my CV."];
   const prompt = "visitor@konstakanniainen.dev ~ $";
   const cursor = "▋";
 
-  const inputRef = useRef<HTMLInputElement>(null);
-
   return (
-    <div className="flex-none max-w-3xl truncate whitespace-pre-line h-96 max-h-full border border-slate-100 bg-black text-white rounded-xl leading-tight text-xs font-[family-name:var(--font-geist-mono)]">
+    <div
+      className="flex-none max-w-3xl truncate whitespace-pre-line h-96 max-h-full border border-slate-100 bg-black text-white rounded-xl leading-tight text-xs font-[family-name:var(--font-geist-mono)]"
+      onClick={(e) => {
+        e.stopPropagation();
+        setInputInFocus(true);
+      }}
+    >
       <div className="flex flex-row h-6 py-1 px-1 bg-neutral-700">
         <div className="px-1 border-r">x</div>
         <div className="px-1 border-r">-</div>
